1199da1048489249b69ae43c2a429aa15968e864
izuru775 <izuru775@gmail.com>
2022-04-15 09:52:59 +1000

    Dislike added

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -31,17 +31,20 @@
                             {{ $post->body }}</p>
                     </div>
                     <div class="flex item-center">
-                        @if (!$post->likedBy(auth()->user()))
-                            <form action="{{ route('posts.likes', $post->id) }}" method="post" class="mr-1">
-                                @csrf
-                                <button type="submit" class="text-blue-500">Like</button>
-                            </form>
-                        @else
-                            <form action="" method="post" class="mr-1">
-                                @csrf
-                                <button type="submit" class="text-blue-500">Unlike</button>
-                            </form>
-                        @endif
+                        @auth
+                            @if (!$post->likedBy(auth()->user()))
+                                <form action="{{ route('posts.likes', $post) }}" method="post" class="mr-1">
+                                    @csrf
+                                    <button type="submit" class="text-blue-500">Like</button>
+                                </form>
+                            @else
+                                <form action="{{ route('posts.likes', $post) }}" method="post" class="mr-1">
+                                    @csrf
+                                    @method('DELETE')
+                                    <button type="submit" class="text-blue-500">Unlike</button>
+                                </form>
+                            @endif
+                        @endauth
                         <span>{{ $post->likes->count() }} {{ Str::plural('like', $post->likes->count()) }}</span>
                     </div>
                 @endforeach
