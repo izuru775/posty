47eac9395ec339eb5c31b13200607e03abc7742b
izuru775 <izuru775@gmail.com>
2022-04-20 16:22:56 +1000

    Likes configured

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -10,10 +10,16 @@ class PostController extends Controller
     public function index()
     {
         
-        $posts = Post::with(['user','likes'])->paginate(20);
+        $posts = Post::latest()->with(['user','likes'])->paginate(20);
 
         return view('posts.index',[
             'posts'=>$posts
+        ]);
+    }
+    public function show(Post $post)
+    {
+        return view('posts.show',[
+            'post'=>$post,
         ]);
     }
     public function store(Request $request)
@@ -25,5 +31,13 @@ class PostController extends Controller
        $request->user()->posts()->create($request->only('body'));
 
        return back();
+    }
+    public function destroy(Post $post)
+    {
+        $this->authorize('delete',$post);
+        
+        $post->delete();
+
+        return back();
     }
 }
--- app/Models/Post.php
+++ app/Models/Post.php
@@ -24,4 +24,5 @@ class Post extends Model
     {
         return $this->hasMany(Like::class);
     }
+    
 }
