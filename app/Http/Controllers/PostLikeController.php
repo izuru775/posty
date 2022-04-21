29f5a470325f612258ee22f76fced0e3656ffc8b
izuru775 <izuru775@gmail.com>
2022-04-21 20:51:14 +1000

    App completed

--- app/Http/Controllers/PostLikeController.php
+++ app/Http/Controllers/PostLikeController.php
@@ -9,6 +9,7 @@ use Illuminate\Support\Facades\Mail;
 
 class PostLikeController extends Controller
 {
+
     public function __construct()
     {
         $this->middleware(['auth']);
@@ -24,15 +25,16 @@ class PostLikeController extends Controller
             'user_id' => $request->user()->id,
         ]);
 
-        
-        Mail::to($post->user)->send(new PostLiked(auth()->user(),$post));
-        
+        if (!$post->likes()->onlyTrashed()->where('user_id',$request->user()->id)->count()) {
+            Mail::to($post->user)->send(new PostLiked(auth()->user(), $post));
+        }
+
 
         return back();
     }
     public function destroy(Post $post, Request $request)
     {
-        $request->user()->likes()->where('post_id',$post->id)->delete();
+        $request->user()->likes()->where('post_id', $post->id)->delete();
 
         return back();
     }
